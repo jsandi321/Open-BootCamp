392a0f957203bdfe993a8c26b9a38e2f7e667e19
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-06-10 23:06:31 -0600

    Se agrega descripcion del ejercicio

--- Java Basico/leccion01/src/com/example/Main.java
+++ Java Basico/leccion01/src/com/example/Main.java
@@ -3,7 +3,14 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         //Primer ejercicio Open Bootcamp Java Básico Backend Road
+        /*
+        Para este primer ejercicio tendréis que realizar lo siguiente:
 
+        Crea un proyecto de Java desde 0
+        Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+        Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+        */
+        
         //Datos numericos
         int entero = 50;
         System.out.println(entero);
