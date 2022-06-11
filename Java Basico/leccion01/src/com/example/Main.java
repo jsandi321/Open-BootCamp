c32970ec3e3490e1f4192640fb5e0ebfb1ab5c7d
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-06-10 23:04:31 -0600

    Add files via upload

---
+++ Java Basico/leccion01/src/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Primer ejercicio Open Bootcamp Java Básico Backend Road
+
+        //Datos numericos
+        int entero = 50;
+        System.out.println(entero);
+        long numLong = 89273421;
+        System.out.println(numLong);
+        float decimal1 = 17.4f;
+        System.out.println(decimal1);
+        double decimal2 = 99.4;
+        System.out.println(decimal2);
+
+        //Booleanos
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+
+        //Texto
+        char caracter = 'a';
+        System.out.println(caracter);
+        String saludo = "Hola";
+        System.out.println(saludo);
+    }
+}
