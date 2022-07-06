1d9e3b1199febab8498df181a2b64c002aade866
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-07-06 13:06:51 -0600

    Ejercicio 01 con Spring
    Crear proyecto maven con la dependencia spring-context y crear una clase Saludo con un método imprimirSaludo que imprima un texto por consola.
    
    Crear el archivo beans.xml con un bean para la clase Saludo.
    
    Obtener el bean desde el método main y ejecutar el método imprimirSaludo.

---
+++ Spring/ejercicio01/src/main/java/Saludo.java
@@ -0,0 +1,10 @@
+public class Saludo {
+
+    Saludo(){
+        System.out.println("Constructor de la clase");
+    }
+
+    public void imprimirSaludo(){
+        System.out.println("Hola desde la clase Saludo");
+    }
+}
