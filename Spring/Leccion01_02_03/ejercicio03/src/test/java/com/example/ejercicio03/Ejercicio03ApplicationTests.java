f66abcca1a1c989e1a7983e96d28b93883a21b9e
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-07-10 22:13:00 -0600

    Avance ejercicio 03
    Avance ejercicio 03 con Spring Boot

---
+++ Spring/Leccion01_02_03/ejercicio03/src/test/java/com/example/ejercicio03/Ejercicio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
