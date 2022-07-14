fc491cf973bcaca8d5aba5a7b75e69309c32e93c
jsandi321 <jsandi321@gmail.com>
2022-07-13 18:34:39 -0600

    Se completa ejercicio03

--- Spring/Leccion04_05_06/ejercicio01/src/main/java/com/example/ejercicio01/controller/LaptopController.java
+++ Spring/Leccion04_05_06/ejercicio01/src/main/java/com/example/ejercicio01/controller/LaptopController.java
@@ -4,6 +4,8 @@ package com.example.ejercicio01.controller;
 import com.example.ejercicio01.entities.Laptop;
 import com.example.ejercicio01.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,5 +25,12 @@ public class LaptopController {
     public List<Laptop> findAll(){
         //Recuperar y devolver todas las laptos en base de datos
         return laptopRepository.findAll();
+    }
+
+    //Metodo Request
+    @PostMapping("/api/laptops")
+    public Laptop  createBook(@RequestBody Laptop laptop){
+        //Guarda en base de datos un objeto laptop desde un JSON
+        return laptopRepository.save(laptop);
     }
 }
